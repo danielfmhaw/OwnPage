23d9a16be7028cad2d0010fbc948004bf5156fcf
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-05-19 11:45:06 +0200

    removes unused dependencies

--- frontend-testing/src/test/java/com/example/frontend/steps/RegisterSteps.java
+++ frontend-testing/src/test/java/com/example/frontend/steps/RegisterSteps.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import com.example.frontend.utils.WebDriverUtils;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.WebElement;
 import java.util.List;
 
