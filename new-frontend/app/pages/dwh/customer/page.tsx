e866f81561e913fa6355eef4f6238450f1656f82
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-06-15 22:10:02 +0200

    adds id to data table and triggerReload to layout

--- new-frontend/app/pages/dwh/customer/page.tsx
+++ new-frontend/app/pages/dwh/customer/page.tsx
@@ -15,13 +15,11 @@ import type {FilterDefinition} from "@/components/helpers/FilterBar";
 import ContentLayout from "@/components/layout/ContentLayout";
 import CustomerDetailContent from "@/pages/dwh/customer/customer-detail-content";
 import AddCustomerContent from "@/pages/dwh/customer/add-customer-content";
-import {useReloadedData} from "@/models/datatable/reloadState";
 
 export default function CustomerPage() {
     const {t} = useTranslation();
     const {addNotification} = useNotification();
     const refreshData = useRefreshData(itemsLoader);
-    useReloadedData(itemsLoader, "customer");
 
     const [data, setData] = React.useState<Customer[]>([]);
     const [totalCount, setTotalCount] = React.useState<number>(0);
@@ -155,6 +153,7 @@ export default function CustomerPage() {
     return (
         <ContentLayout title={t("customer.data")}>
             <DataTable
+                url="customer"
                 title={t("customer.data")}
                 columns={columns}
                 data={data}
