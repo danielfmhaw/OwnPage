5db347778ed80f2aa09a44724ee7f149e3b8ae7a
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-06-17 18:39:42 +0200

    renames vite-app to new-frontend and deletes old version

--- vite-app/src/models/api/models/DeleteEmailParam.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * Email address of the user to retrieve or modify
- */
-export type DeleteEmailParam = string;
--- vite-app/src/models/api/models/OrderBy.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * Specifying the sort order. The sort definition can use the keywords 'asc' for ascending and 'desc' for descending sort order.
- */
-export type OrderBy = string;
