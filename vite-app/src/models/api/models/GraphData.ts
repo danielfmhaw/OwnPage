764edaddd601924616658c0bfe0cbff1730d6fd0
Daniel Freire Mendes <danielfmhaw@gmail.com>
2025-06-17 16:19:00 +0200

    adds models

---
+++ vite-app/src/models/api/models/GraphData.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type GraphData = {
+    /**
+     * Time bucket (e.g., week, month)
+     */
+    bucket: string;
+    /**
+     * Revenue for the bucket
+     */
+    revenue: number;
+    /**
+     * Number of sales in the bucket
+     */
+    sales_no: number;
+};
+
